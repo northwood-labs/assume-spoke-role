f4b184821052408368c51e3eb810045183587db3
Ryan Parman <ryan@ryanparman.com>
2022-03-24 14:47:07 -0600

    Switch to input object.

--- cmd_run.go
+++ cmd_run.go
@@ -22,20 +22,22 @@ take precedence over environment variables.`
 	)
 
 	var (
-		externalID   = os.Getenv("ASSUME_ROLE_EXTERNAL_ID")
-		hubAccount   = os.Getenv("ASSUME_ROLE_HUB_ACCOUNT")
-		hubRole      = os.Getenv("ASSUME_ROLE_HUB_ROLE")
-		spokeAccount = os.Getenv("ASSUME_ROLE_SPOKE_ACCOUNT")
-		spokeRole    = os.Getenv("ASSUME_ROLE_SPOKE_ROLE")
+		externalID    = os.Getenv("ASSUME_ROLE_EXTERNAL_ID")
+		hubAccount    = os.Getenv("ASSUME_ROLE_HUB_ACCOUNT")
+		hubRole       = os.Getenv("ASSUME_ROLE_HUB_ROLE")
+		spokeAccount  = os.Getenv("ASSUME_ROLE_SPOKE_ACCOUNT")
+		spokeRole     = os.Getenv("ASSUME_ROLE_SPOKE_ROLE")
+		sessionString = os.Getenv("ASSUME_ROLE_SESSION_STRING")
 
-		externalIDFlag   = cmd.StringOpt("e external-id", externalID, "(ASSUME_ROLE_EXTERNAL_ID) The external ID value that is required by your hub and spoke policies, if any.") // lint:ignore-length
-		hubAccountFlag   = cmd.StringOpt("h hub-account", hubAccount, "(ASSUME_ROLE_HUB_ACCOUNT) The 12-digit AWS account ID containing the HUB policy.")                         // lint:ignore-length
-		spokeAccountFlag = cmd.StringOpt("s spoke-account", spokeAccount, "(ASSUME_ROLE_SPOKE_ACCOUNT) The 12-digit AWS account ID containing the SPOKE policy.")                 // lint:ignore-length
-		hubRoleFlag      = cmd.StringOpt("H hub-role", hubRole, "(ASSUME_ROLE_HUB_ROLE) The name of the IAM role to assume in the HUB account.")                                  // lint:ignore-length
-		spokeRoleFlag    = cmd.StringOpt("S spoke-role", spokeRole, "(ASSUME_ROLE_SPOKE_ROLE) The name of the IAM role to assume in the HUB account.")                            // lint:ignore-length
-		retriesFlag      = cmd.IntOpt("r retries", defaultAWSRetries, "The maximum number of retries that the underlying AWS SDK should perform.")                                // lint:ignore-length
-		verboseFlag      = cmd.BoolOpt("v verbose", false, "Enable verbose logging.")                                                                                             // lint:ignore-length
-		cmdd             = cmd.StringsArg("COMMAND", []string{""}, "The command to run using the spoke policy.")
+		externalIDFlag    = cmd.StringOpt("e external-id", externalID, "(ASSUME_ROLE_EXTERNAL_ID) The external ID value that is required by your hub and spoke policies, if any.")   // lint:ignore-length
+		hubAccountFlag    = cmd.StringOpt("h hub-account", hubAccount, "(ASSUME_ROLE_HUB_ACCOUNT) The 12-digit AWS account ID containing the HUB policy.")                           // lint:ignore-length
+		spokeAccountFlag  = cmd.StringOpt("s spoke-account", spokeAccount, "(ASSUME_ROLE_SPOKE_ACCOUNT) The 12-digit AWS account ID containing the SPOKE policy.")                   // lint:ignore-length
+		hubRoleFlag       = cmd.StringOpt("H hub-role", hubRole, "(ASSUME_ROLE_HUB_ROLE) The name of the IAM role to assume in the HUB account.")                                    // lint:ignore-length
+		spokeRoleFlag     = cmd.StringOpt("S spoke-role", spokeRole, "(ASSUME_ROLE_SPOKE_ROLE) The name of the IAM role to assume in the HUB account.")                              // lint:ignore-length
+		sessionStringFlag = cmd.StringOpt("session-string", sessionString, "(ASSUME_ROLE_SESSION_STRING) A string that will be part of the resulting User ID in the spoke account.") // lint:ignore-length
+		retriesFlag       = cmd.IntOpt("r retries", defaultAWSRetries, "The maximum number of retries that the underlying AWS SDK should perform.")                                  // lint:ignore-length
+		verboseFlag       = cmd.BoolOpt("v verbose", false, "Enable verbose logging.")                                                                                               // lint:ignore-length
+		cmdd              = cmd.StringsArg("COMMAND", []string{""}, "The command to run using the spoke policy.")
 	)
 
 	cmd.Spec = `[-e=<external-id>] [-h=<hub-account>] [-s=<spoke-account>] [-H=<hub-role>] ` +
@@ -49,15 +51,15 @@ take precedence over environment variables.`
 		}
 
 		// Assume appropriate roles and return session credentials for the "Spoke" account.
-		roleCredentials, _, err := hubspoke.GetSpokeCredentials(
-			ctx,
-			&config,
-			*hubAccountFlag,
-			*spokeAccountFlag,
-			*hubRoleFlag,
-			*spokeRoleFlag,
-			*externalIDFlag,
-		)
+		roleCredentials, _, err := hubspoke.GetSpokeCredentials(hubspoke.SpokeCredentialsInput(
+            Context: ctx,
+            Config: &config,
+            HubAccountFlag: *hubAccountFlag,
+            SpokeAccountFlag: *spokeAccountFlag,
+            HubRoleFlag: *hubRoleFlag,
+            SpokeRoleFlag: *spokeRoleFlag,
+            ExternalIDFlag: *externalIDFlag,
+        ))
 		if err != nil {
 			exiterrorf.ExitErrorf(errors.Wrap(err, "could not generate valid AWS credentials for the 'spoke' account"))
 		}
--- hubspoke/functions.go
+++ hubspoke/functions.go
@@ -13,58 +13,83 @@ import (
 	errors "github.com/go-faster/errors"
 )
 
-// GetSpokeCredentials accepts an AWS Config object, a hub and spoke account ID,
-// a hub and spoke role name, and returns a set of STS session credentials for
-// the spoke account.
-func GetSpokeCredentials(
-	ctx context.Context,
-	config *aws.Config,
-	hubAccountID,
-	spokeAccountID,
-	hubRoleName,
-	spokeRoleName,
-	externalID string,
-) (*types.Credentials, aws.Config, error) {
+// SpokeCredentialsInput is an input object for the GetSpokeCredentials
+// function.
+type SpokeCredentialsInput struct {
+	// (Required) A context.Context object.
+	Context context.Context
+
+	// (Required) An AWS SDK v2 configuration object.
+	Config *aws.Config
+
+	// (Required) The AWS Account ID of the "hub" account.
+	HubAccountID string
+
+	// (Required) The AWS Account ID of the "spoke" account.
+	SpokeAccountID string
+
+	// (Required) The name of the role to assume inside the "hub" account.
+	HubRoleName string
+
+	// (Required) The name of the role to assume inside the "spoke" account.
+	SpokeRoleName string
+
+	// (Optional) A string identifier that should match what the IAM policies
+	// require, if anything. If empty, an empty string will be passed along.
+	ExternalID string
+
+	// (Optional) A string identifier to use to represent the
+	// user/software/role, and will show up under the `UserId` result of `aws
+	// sts get-caller-identity`. If empty, a random string will be generated.
+	SessionString string
+}
+
+// GetSpokeCredentials accepts a GetSpokeCredentialsInput object, and returns a
+// set of STS session credentials for the spoke account.
+func GetSpokeCredentials(o SpokeCredentialsInput) (*types.Credentials, aws.Config, error) {
 	emptyCredentials := types.Credentials{}
 	emptyConfig := aws.Config{}
 
-	rand.Seed(time.Now().UnixNano())
+	sessionName := o.SessionString
+	if sessionName == "" {
+		rand.Seed(time.Now().UnixNano())
+		sessionName = randomString(32)
+	}
 
-	sessionName := randomString(32)
-	hubRoleARN := fmt.Sprintf("arn:aws:iam::%s:role/%s", hubAccountID, hubRoleName)
-	spokeRoleARN := fmt.Sprintf("arn:aws:iam::%s:role/%s", spokeAccountID, spokeRoleName)
+	hubRoleARN := fmt.Sprintf("arn:aws:iam::%s:role/%s", o.HubAccountID, o.HubRoleName)
+	spokeRoleARN := fmt.Sprintf("arn:aws:iam::%s:role/%s", o.SpokeAccountID, o.SpokeRoleName)
 
 	// Assume the HUB role.
-	stsHubClient := sts.NewFromConfig(*config)
-	config.Credentials = aws.NewCredentialsCache(
+	stsHubClient := sts.NewFromConfig(*o.Config)
+	o.Config.Credentials = aws.NewCredentialsCache(
 		stscreds.NewAssumeRoleProvider(stsHubClient, hubRoleARN, func(o *stscreds.AssumeRoleOptions) {
-			o.ExternalID = aws.String(externalID)
+			o.ExternalID = aws.String(*o.ExternalID)
 		}),
 	)
 
 	// Assume the SPOKE role.
-	stsSpokeClient := sts.NewFromConfig(*config)
-	response, err := stsSpokeClient.AssumeRole(ctx, &sts.AssumeRoleInput{
+	stsSpokeClient := sts.NewFromConfig(*o.Config)
+	response, err := stsSpokeClient.AssumeRole(o.Context, &sts.AssumeRoleInput{
 		RoleArn:         aws.String(spokeRoleARN),
-		RoleSessionName: aws.String(fmt.Sprintf("%s-%s", spokeAccountID, sessionName)),
-		ExternalId:      aws.String(externalID),
+		RoleSessionName: aws.String(fmt.Sprintf("%s-%s", o.SpokeAccountID, sessionName)),
+		ExternalId:      aws.String(o.ExternalID),
 	})
 	if err != nil { // lint:allow_cuddling
 		return &emptyCredentials, emptyConfig, errors.Wrap(err, fmt.Sprintf(
 			"error assuming '%s' role in account %s",
 			spokeRoleARN,
-			spokeAccountID,
+			o.SpokeAccountID,
 		))
 	}
 
-	config.Credentials = aws.NewCredentialsCache(
+	o.Config.Credentials = aws.NewCredentialsCache(
 		stscreds.NewAssumeRoleProvider(stsSpokeClient, spokeRoleARN, func(o *stscreds.AssumeRoleOptions) {
-			o.RoleSessionName = fmt.Sprintf("%s-%s", spokeAccountID, sessionName)
-			o.ExternalID = aws.String(externalID)
+			o.RoleSessionName = fmt.Sprintf("%s-%s", o.SpokeAccountID, sessionName)
+			o.ExternalID = aws.String(*o.ExternalID)
 		}),
 	)
 
-	return response.Credentials, *config, nil
+	return response.Credentials, *o.Config, nil
 }
 
 func randomString(l int) string {
